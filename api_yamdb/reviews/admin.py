37a18a3005179688ec7068ab4673217e0bf981e3
Vlad <stas.basov@whatever.com>
2023-07-12 18:16:56 +0300

    fixes

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -2,23 +2,6 @@ from django.contrib import admin
 from django.contrib.admin.options import TabularInline
 
 from .models import Category, Genre, GenreTitle, Review, Title, Comment
-from users.models import User
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = (
-        'username',
-        'email',
-        'role',
-        'bio',
-        'first_name',
-        'last_name',
-        'confirmation_code',
-    )
-    search_fields = ('username', 'role',)
-    list_filter = ('username',)
-    empty_value_display = '-пусто-'
-
 
 class GenreTitleoInline(TabularInline):
     model = GenreTitle
